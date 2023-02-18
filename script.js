b1eadecb558b26bf00f46ab2eae3a6255d9e2e95
Sabbath4 <123818149+Sabbath4@users.noreply.github.com>
2023-02-18 22:32:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let movies = [
+  {
+	"name": "Iron man",
+	"Distributed by": "Paramount Pictures",
+    "Running time" : "126"
+  },
+  {
+	"name": "Fight Club",
+	"Distributed by": "20th Century Studios, Inc",
+    "Running time" : "139"
+  },
+  {   "name": "Forrest Gump",
+	"Distributed by": "Paramount Pictures",
+    "Running time" : "142"
+  }
+]
+
+
+	
+	
