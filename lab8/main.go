bf0e5cdc6bd9794738369700878c5db6cd5532e5
Sabbath4 <123818149+Sabbath4@users.noreply.github.com>
2023-04-13 23:48:40 +0600

    Add files via upload

---
+++ lab8/main.go
@@ -0,0 +1,17 @@
+package main
+import (
+    "fmt"
+    "net/http"
+)
+  
+func main() {
+ 
+    http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request){
+        http.ServeFile(w, r, "static/about.html")
+    })
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+        http.ServeFile(w, r, "static/index.html")
+    })
+    fmt.Println("Server is listening...")
+    http.ListenAndServe(":8181", nil)
+}
